4dd7e4e6404d03b92275fea4ccc7f7bbc52b7e70
siddhardhan s <codewithsidfiles23@gmail.com>
2025-04-18 21:24:36 +0530

    frontend changes for model selection

--- app.py
+++ app.py
@@ -1,29 +1,48 @@
-import streamlit as st
 import requests
+import streamlit as st
 
+
+# --- page Configuration ---
 st.set_page_config(
     page_title="StudyPal",
     page_icon="🤖",
     layout="centered" # wide
 )
-
 st.title("🤖 StudyPal Application")
 
-api_url = "http://127.0.0.1:8000/ask"
+# --- API Details ---
+API_URL = "http://127.0.0.1:8000/ask"
+PROVIDERS = ["Groq", "Ollama"]
+MODELS = ["llama 3.3", "DeepSeek R1"]
 
-# get user input
-user_question = st.text_input(label="Ask your question", placeholder="eg. What is ML?")
+# --- Initialize Session State ---
+st.session_state.setdefault("show_settings", False)
+st.session_state.setdefault("provider", PROVIDERS[0])
+st.session_state.setdefault("model", MODELS[0])
 
-# button to trigger the API
+
+# --- Layout ---
+col1, col2 = st.columns([8, 1])
+question = col1.text_input(label="Ask your question", placeholder="eg. What is ML?")
+
+if col2.button("⚙️", help="Select Model Settings"):
+    st.session_state.show_settings = not st.session_state.show_settings
+
+# --- Model Settings ---
+if st.session_state.show_settings:
+    with st.expander("🔧 Model Settings", expanded=True):
+        st.session_state.provider = st.selectbox(label="Provider",
+                                                 options=PROVIDERS,
+                                                 index=PROVIDERS.index(st.session_state.provider))
+        st.session_state.model = st.selectbox(label="Model",
+                                              options=MODELS,
+                                              index=MODELS.index(st.session_state.model))
+        st.success(f"Using {st.session_state.provider} - {st.session_state.model}")
+
+
+# --- Get Answer form API ---
 if st.button("Get Answer"):
-
-    response = requests.post(
-        url = api_url,
-        json = {"question": user_question}
-    )
-
+    response = requests.post(url=API_URL, json={"question": question})
     result = response.json()
     answer = result["answer"]
-
     st.success(answer)
-
